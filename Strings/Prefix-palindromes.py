cb2c54efed01a162258d78d8052dd07dab283efa
alesanm <alesan.med@yandex.ru>
2023-03-11 23:49:44 +0100

    Prefix-palindromes

---
+++ Strings/Prefix-palindromes.py
@@ -0,0 +1,18 @@
+
+def prefix(s):
+    n = len(s)
+    b = [0 for i in range(n)]
+    for i in range(1, n):
+        k = b[i - 1]
+        while k > 0 and s[k] != s[i]:
+            k = b[k - 1]
+        if s[k] == s[i]:
+            b[i] = k + 1
+    return b
+
+
+if __name__ == '__main__':
+    s = input()
+    # s+#+reverse(s).
+
+    print(prefix(s+'#'+s[::-1])[-1])
